4ea823e9e5d57434d0570787b11c8e59d0f90627
Omphulusa <omphu.shau@outlook.com>
2023-02-16 10:01:38 -0800

    The hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - puts string on the screen
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
