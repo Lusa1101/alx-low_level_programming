53eb361bab6132df745de149d6e7da23e30ab534
Omphulusa <omphu.shau@outlook.com>
2023-02-16 21:53:04 +0000

    The hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - prints a message using fprintf
  *
- * return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\"");
+fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
 return (1);
 }
