b0790b348f3810f337c7fdc67331bc107dc0e465
Omphulusa <omphu.shau@outlook.com>
2023-02-17 15:42:33 -0800

    The 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - prints word in both cases
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int x = 'A';
+while (x <= 'Z')
+{
+x = tolower(x);
+putchar(x);
+x++;
+}
+while (x <= 'Z')
+{
+x = toupper(x);
+putchar(x);
+x++;
+}
+return (0);
+}
