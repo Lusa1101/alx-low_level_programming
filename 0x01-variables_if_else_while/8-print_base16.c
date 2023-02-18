c3cc31a695a9b70be1e18da1a7365408132c5e46
Omphulusa <omphu.shau@outlook.com>
2023-02-18 06:14:12 -0800

    The 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - prints the base 16 characters
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+int n = 0;
+
+while (n < 16)
+{
+if (n < 10)
+putchar(n % 10 + '0');
+else
+putchar(n + 87);
+n++;
+}
+putchar('\n');
+return (0);
+}
