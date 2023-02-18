fe8afb7488a7fe8898188b5045310a5717cb7803
Omphulusa <omphu.shau@outlook.com>
2023-02-18 06:34:52 -0800

    The 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible single digit combination
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+putchar(n % 10 + '0');
+if (n == 9)
+break;
+putchar(',');
+putchar(' ');
+n++;
+}
+putchar('\n');
+return (0);
+}
+      
