2be712085219f747e9ec0e102a3e28bccc8fb0f7
Omphulusa <omphu.shau@outlook.com>
2023-02-17 16:34:11 -0800

    The 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print the base-10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+printf("%d",n);
+n++;
+}
+putchar('\n');
+return (0);
+}
