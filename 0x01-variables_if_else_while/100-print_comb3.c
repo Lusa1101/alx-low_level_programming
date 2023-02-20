96db17839b4c3a03bc9d56d4c9c891990ebd3343
Omphulusa <omphu.shau@outlook.com>
2023-02-20 05:44:30 -0800

    The 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,25 @@
 int main(void)
 {
 int n = 0, m = 0;
-
+int x = 9, count = 0;
 while (n < 9)
 {
-putchar(n % 10 + '0');
+m = (9 - x);
 while (m < 10)
 {
+if (n != m)
+{
+putchar(n % 10 + '0');
 putchar(m % 10 + '0');
-if (n == 8 && m == 9)
+if (count == 44)
 break;
 putchar(',');
 putchar(' ');
+count++;
+}
 m++;
 }
+x--;
 n++;
 }
 putchar('\n');
