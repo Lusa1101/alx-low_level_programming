edf8342b5be9b66ba73c3245363240bb9799a3e3
Omphulusa <omphu.shau@outlook.com>
2023-02-21 12:04:22 -0800

    Functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include "_putchar.h"
+#include "main.h"
 
+/**
+ * main - prints char* using user-defined header
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-  char var[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-  int i = 0;
+char var[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+int i = 0;
 
-  while (i < 9)
-    {
-      _putchar(var[i]);
-    }
+while (i < 9)
+{
+_putchar(var[i]);
+}
 
-  return (0);
+return (0);
 }
