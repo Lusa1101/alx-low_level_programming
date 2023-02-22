7661e4386199e1265ff9dd841f3b03947cd79a29
Omphulusa <omphu.shau@outlook.com>
2023-02-22 06:44:51 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ *
+ * Return: Alway 0 for success
+ */
+int _islower(int c)
+{
+if (65 <= c <= 90)
+return (1);
+else
+return (0);
+}
