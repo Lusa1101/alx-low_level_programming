ab2f0ac730ff88be396e2e82faa1a434d50a1059
Omphulusa <omphu.shau@outlook.com>
2023-02-22 04:11:49 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabets 10 times
+ *
+ * Return: Always 1
+ */
+void print_alphabets_x10(void)
+{
+int i = 65, count = 0;
+
+while (count != 10)
+{
+while (i < 91)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
