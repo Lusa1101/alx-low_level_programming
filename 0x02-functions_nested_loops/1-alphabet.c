eaeb1df2215e2f2d6203fa1493a933909aa2197e
Omphulusa <omphu.shau@outlook.com>
2023-02-22 03:08:30 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabets
+ *
+ * Return: ALways 0
+ */
+int main(void)
+{
+int i = 65;
+
+while (i < 9)
+{
+_putchar(i);
+}
+_putchar('\n');
+
+return (0);
+}
