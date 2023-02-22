7ce1cea816096dda91d8e8aadd4705f0d53c88be
Omphulusa <omphu.shau@outlook.com>
2023-02-22 14:00:45 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints number to 98
+ *
+ * Return: Always the value
+ */
+void print_to_98(int n)
+{
+  while (n <= 98)
+    {
+      int n = n + 1;
+      _putchar(n % 10 + '0');
+
+      if (n != 98)
+	{
+	  _putchar(',');
+	  _putchar(' ');
+	}
+      n++;
+     )
+    while ( n => 98)
+      {
+	n = n - 1;
+	_putchar(n % 10 + '0');
+
+	if (n != 98)
+	  {
+	    _putchar(',');
+	    _putchar(' ');
+	  }
+	n--;
+      }
+}
