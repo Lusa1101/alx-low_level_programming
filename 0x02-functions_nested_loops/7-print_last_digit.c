3b7c969c41278a6fe9cd517531b7622c8e4f063d
Omphulusa <omphu.shau@gmail.com>
2023-02-22 19:31:37 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - will print the last digit of any number given
+ *
+ * Return: Always returns the solution
+ */
+int print_last_digit(int c)
+{
+return (c % 10);
+}
