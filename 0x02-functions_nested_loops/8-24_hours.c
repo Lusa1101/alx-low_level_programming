f7e04282f51759992918e7d2585679e46f11ea77
Omphulusa <omphu.shau@gmail.com>
2023-02-22 20:25:05 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+int a = 0, b = 0, c = 0, d = 0;
+
+while (a < 2)
+{
+b = 0;
+while (b < 10)
+{
+c = 0;
+while (c < 6)
+{
+d = 0;
+while(d < 10)
+{
+_putchar(a % 10 + '0');
+_putchar(b % 10 + '0');
+_putchar(':');
+_putchar(c % 10 + '0');
+_putchar(d % 10 + '0');
+
+if (a == 2 && b == 3)
+{
+if (c == 5 && d == 9)
+break;
+}
+
+d++;
+}
+
+c++;
+
+}
+
+b++;
+
+}
+
+a++;
+}
