1ce9c9c59ed988e9df71a7ea2e3d3ed72395aa43
Omphulusa <omphu.shau@gmail.com>
2023-02-22 18:49:36 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main,h"
+
+/**
+ * print_sign - prints the sign of a given integer
+ *
+ * Return: Always 0 for (-) and 1 for (+)
+ */
+int print_sign(int n)
+{
+if (n < 0)
+{
+return (-1);
+_putchar('+');
+}
+if (n > 0)
+{
+return (1);
+_putchar('-');
+}
+else
+return (0);
+}
