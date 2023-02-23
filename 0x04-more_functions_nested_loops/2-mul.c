6e47247fea638e1fccaf159aa943d8287f80717e
Omphulusa <omphu.shau@outlook.com>
2023-02-23 12:41:45 -0800

    The more on fuctions nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#inlcude "main.h"
+
+/**
+ * mul - multiplies two given numbers
+ * @a: the first parameter
+ * @b: second parameter
+ * Return: the solution
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
