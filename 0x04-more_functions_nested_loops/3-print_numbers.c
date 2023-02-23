59ef9aad5f1a9e989d39db4c7d84305e89b32245
Omphulusa <omphu.shau@outlook.com>
2023-02-23 12:55:22 -0800

    The more on fuctions nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints out digits
+ *
+ * Return: digits
+ */
+void print_numbers(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+_putchar(1 % 10 + '0');
+i++;
+}
+}
