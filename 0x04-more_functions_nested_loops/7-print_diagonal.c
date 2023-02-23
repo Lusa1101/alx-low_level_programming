253398a4a3a9633def4fada3d0b82ec2ba9fae19
Omphulusa <omphu.shau@outlook.com>
2023-02-23 13:57:11 -0800

    The more on fuctions nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,19 @@
  */
 void print_diagonal(int n)
 {
+int i = 0;
+
 while (n > 0)
 {
+while (i < n)
+{
 _putchar(' ');
-n--;
+i++;
 }
 
-if (!(n <= 0))
 _putchar('\\');
 
+n--;
+}
 _putchar('\n');
 }
