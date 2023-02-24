bdfc3fdd67c0f158ab7cd5f28c4160e862ef87a5
Omphulusa <omphu.shau@outlook.com>
2023-02-24 06:42:50 -0800

    The more on fuctions nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz-buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the fizz buzz test
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int j = 1;
+
+while (j < n + 1)
+{
+if (j % 3 == 0)
+printf("Fizz ");
+else if (j % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ",j);
+j++;
+}
+
+return (0);
+}
