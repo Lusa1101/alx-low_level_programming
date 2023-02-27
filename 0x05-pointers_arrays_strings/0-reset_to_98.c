18d68b1173799fccb49b14d26f02035b7dbd9e4d
Omphulusa <omphu.shau@gmail.com>
2023-02-27 16:51:25 +0000

    Pointers in strings and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - will change the value of n to 98
+ * @n: The parameter to be changed
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+n = 98;
+}
+
