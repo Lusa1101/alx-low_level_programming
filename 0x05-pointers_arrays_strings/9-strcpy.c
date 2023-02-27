5e93c58b62257dfe43326b3ed9ee1493ec1b22de
Omphulusa <omphu.shau@gmail.com>
2023-02-27 21:01:07 +0000

    Pointers in strings and arrays

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: points to the buffer
+ * @src: point to string being copied
+ * Return: string that was copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char str;
+
+str = *src;
+
+*dest = str;
+
+return (*dest);
+}
