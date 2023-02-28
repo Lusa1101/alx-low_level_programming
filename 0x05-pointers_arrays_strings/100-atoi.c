82a783f1f0a2c8b5cfcc397470a51494046ca13c
Omphulusa <omphu.shau@gmail.com>
2023-02-28 18:08:12 +0000

    Pointers in strings and arrays

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - function to convert a string to integer
+ * @s: the string to be converted into an integer
+ * Return: Nothing
+ */
+
+int _atoi(char *s)
+{
+int i = 0;
+char str;
+
+while (s[i] != '\0')
+{
+
+int j = 0;
+
+if (s[i] / 1 == s[i])
+{
+str[j] = s[i];
+j++;
+}
+}
+
+return (str);
+}
