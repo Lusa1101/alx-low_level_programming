57998601c31820a1f29ffaf42ba4316318bf4ba0
Omphulusa <omphu.shau@outlook.com>
2023-03-01 13:16:34 -0800

    More on pointers, strings and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - appends two string together
+ * @dest: one of the strings
+ * @src: second string
+ * Return: The appended string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *str;
+int len = 0;
+
+while (dest[len] != '\0')
+{
+len++;
+}
+
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[len] = src[i];
+len++;
+i++;
+}
+
+dest[len] = '\0';
+
+return (dest);
+}
