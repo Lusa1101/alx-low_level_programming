b1499a06e9c79e34a5165313e11e6fc8a60343a6
Omphulusa <omphu.shau@outlook.com>
2023-03-01 22:47:55 -0800

    More on pointers, strings and arrays

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+11;rgb:0000/0000/0000#include "main.h"
+
+/**
+ * _strncat - function appends two strings at most n bytes
+ * @dest: one of the strings
+ * @src: second string
+ * @n: number of bytes
+ * Return: the appended strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int len = 0, i = 0;
+
+while (dest[len] != '\0')
+{
+len++;
+}
+
+while (src[i] != '\0')
+{
+if (i >= n)
+break;
+
+else
+dest[len] = src[i];
+
+i++;
+len++;
+}
+
+return (dest);
+}
