adb299d3ca676ace6b94f3a72a216cf5f715aa2f
Omphulusa <omphu.shau@outlook.com>
2023-03-01 23:54:30 -0800

    More on pointers, strings and arrays

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return: Always 1 for success
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0, j = 0, n = 0;
+
+while (s1[i] != '\0')
+{
+j = 0;
+while (s2[j] != '\0')
+{
+if (s1[i] != s2[j])
+{
+n = s1[i] - s2[j];
+break;
+}
+else if (s1[i] == s2[j])
+{
+n = 0;
+break;
+}
+j++;
+}
+
+if (n != 1)
+break;
+
+i++;
+}
+
+return (n);
+}
