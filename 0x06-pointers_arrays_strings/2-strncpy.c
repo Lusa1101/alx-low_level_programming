d84703dbaeee13e4dc652230339a6f7379a3e814
Omphulusa <omphu.shau@outlook.com>
2023-03-01 23:20:53 -0800

    More on pointers, strings and arrays

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string that is limited
+ * @dest: string to take the copy
+ * @src: string to be copied
+ * @n: the limit of the string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+if (i >= n)
+break;
+
+
+else
+dest[i] = src[i];
+
+i++;
+}
+
+return (dest);
+}
