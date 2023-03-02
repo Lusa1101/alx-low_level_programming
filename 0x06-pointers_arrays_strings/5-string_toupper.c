6ccebdfdb2f302b3f2c38ff671292b41c8158cd3
Omphulusa <omphu.shau@outlook.com>
2023-03-02 00:29:00 -0800

    More on pointers, strings and arrays

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @str: string parameters
+ * Return: lowercase string
+ */
+
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (str[i] >= 97 && str[i] < 123)
+str[i] -= 32;
+
+i++;
+}
+
+return (str);
+}
