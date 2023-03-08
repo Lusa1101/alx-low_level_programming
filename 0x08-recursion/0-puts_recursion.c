1618b38c3ef463abd6fabf8b480dcee731befdc1
Omphulusa <omphu.shau@outlook.com>
2023-03-08 21:29:08 +0200

    The recursion project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - prints a character
  * _puts - prints a string of characters
  * _puts_recursion - prints a string
- * @c: the character to be prints
  * @str: the string to be printed by _puts
  * @s: the string to be printed by _puts_recursion
  * Return: the string
  */
-
-void _putchar(char c)
-{
-write(1, &c, 1);
-}
 
 void _puts(char *str)
 {
