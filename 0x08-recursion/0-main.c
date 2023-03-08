d038bef325eeb814cab6b89e14e83e8c41b79ab3
Omphulusa <omphu.shau@outlook.com>
2023-03-08 21:27:09 +0200

    The recursion project

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+char *str = "Hi there, old friend. This is a hard course! :)";
+
+_puts_recursion(str);
+
+return (0);
+}
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,36 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
+ * _putchar - prints a character
+ * _puts - prints a string of characters
  * _puts_recursion - prints a string
- * @s: the string to be printed
+ * @c: the character to be prints
+ * @str: the string to be printed by _puts
+ * @s: the string to be printed by _puts_recursion
  * Return: the string
  */
+
+void _putchar(char c)
+{
+write(1, &c, 1);
+}
+
+void _puts(char *str)
+{
+if (str[0] == '\0')
+return;
+
+_putchar(str[0]);
+_puts(str + 1);
+
+}
 
 void _puts_recursion(char *s)
 {
 if (s[0] == '\0')
 return;
 
-_puts(s[0]);
-
-_puts_recursion(s + 1);
+_puts(s);
+_putchar('\n');
 }
-
