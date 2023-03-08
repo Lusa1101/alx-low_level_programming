894c4c6e294afeb1b23c438e6ee25e05cd1f0d25
Omphulusa <omphu.shau@outlook.com>
2023-03-08 21:40:17 +0200

    The recursion project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 /**
- * _puts - prints a string of characters
  * _puts_recursion - prints a string
+ * _puts - prints a string of characters
  * @str: the string to be printed by _puts
  * @s: the string to be printed by _puts_recursion
  * Return: the string
@@ -22,7 +22,10 @@ _puts(str + 1);
 void _puts_recursion(char *s)
 {
 if (s[0] == '\0')
+{
+_puts("\n");
 return;
+}
 
 _puts(s);
 _puts("\n");
