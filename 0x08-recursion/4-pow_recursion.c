93b01e7a9f2c681f55876ec6741f28f5dfe86e9e
Omphulusa <omphu.shau@outlook.com>
2023-03-09 10:44:58 +0200

    The recursion project

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * pow_recursion - calculates the value of x to the power of y
+ * @x: the base
+ * @y: the exponent
+ * Return: the value
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+
+return (x * _pow_recursion(x, y - 1);
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,4 @@ int _strlen(char *c);
 int _strlen_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
