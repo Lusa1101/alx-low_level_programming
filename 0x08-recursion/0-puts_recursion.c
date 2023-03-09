ff5d4913d9793d7a9953e2311608a07d602bd091
Omphulusa <omphu.shau@outlook.com>
2023-03-09 10:25:41 +0200

    The recursion project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,8 +3,6 @@
 
 /**
  * _puts_recursion - prints a string from the main
- * _puts - prints a string
- * @str: the string to be printed in _puts
  * @s: the string to be printed by _puts_recursion
  * Return: the string
  */
