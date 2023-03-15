4cdac4400f6306ebb98221e471e675e2bcf3c7f8
Omphulusa <omphu.shau@outlook.com>
2023-03-15 02:00:49 +0200

    The memory allocation project

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,6 +4,7 @@
 /**
  * _strdup - returns a ptr to newly alloc space with a copy of str
  * @str: the string's ptr
+ * _strlen - returns the length of a string
  * Return: a ptr
  */
 
@@ -18,9 +19,9 @@ return (1 + _strlen(str + 1));
 char *_strdup(char *str)
 {
 char *t;
-int s = _strlen(str), i = 0;
+int s = _strlen(str) - 1, i = 0;
 
-if (str == NULL)
+if (NULL)
 return (NULL);
 
 t = malloc(sizeof(*t) * s);
